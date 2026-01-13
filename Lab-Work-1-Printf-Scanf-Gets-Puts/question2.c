26502d00b82f818087bfa18c0f02863210a513db
Siddharth Srivastav <siddharthsrivastav805@gmail.com>
2026-01-13 17:30:04 +0530

    Add files via upload

---
+++ Lab-Work-1-Printf-Scanf-Gets-Puts/question2.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main()
+{
+   printf("_______________________________________________________________________________\n");
+   printf("S.No.\t\tName\t\tAge in years\t\tHeight\t\tGender\n");
+   printf(" 1.  \t\tRam \t\t21\t\t\t6.2\t\tM\n");
+   printf(" 2.  \t\tSeeta \t\t20\t\t\t5.6\t\tF\n");
+   printf("_______________________________________________________________________________\n");
+   
+   return 0;
+}
