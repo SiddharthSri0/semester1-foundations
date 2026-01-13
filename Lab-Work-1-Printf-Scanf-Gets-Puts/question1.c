608c00db2ea898d877d92931965cff6eaee2f36b
Siddharth Srivastav <siddharthsrivastav805@gmail.com>
2026-01-13 17:15:53 +0530

    Add files via upload

---
+++ Lab-Work-1-Printf-Scanf-Gets-Puts/question1.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+//question 1 
+int main()
+{
+    //part a 
+    printf("ABCDEFHIJKLMNOPQRSTUVWXY");
+    
+    //part b
+    printf(".   A.   \n");
+    printf(".  BCD.  \n");
+    printf(". EFGHI. \n");
+    printf(" JKLMNOP \n");
+    printf("QRSTUVWXY\n");
+    printf("\n");
+    
+    //part c
+    printf("\"QRSTUVWXY\"\n");
+    printf(" \"JKLMNOP\" \n");
+    printf(". \"EFGHI\". \n");
+    printf(".  \"BCD\".  \n");
+    printf(".   \"A\".   \n");
+    
+    
+    
+    return 0;
+}
