dfa79eec146c87de5685f0a3fb247c57736581e6
Siddharth Srivastav <siddharthsrivastav805@gmail.com>
2026-01-14 15:48:31 +0530

    Add files via upload

---
+++ Lab-Work-3_if-else_swith-case_nested-if-else/question1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Enter your number \n");
+    int num;
+    scanf("%d",&num);
+    if("num%2==0")
+    printf("your number was even\n");
+    else 
+    printf("your number was odd\n");
+
+    return 0;
+}
