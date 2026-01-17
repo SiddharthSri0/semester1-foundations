80c8427a76ee9ee629c7333e7165d8a6ebda6331
Siddharth Srivastav <siddharthsrivastav805@gmail.com>
2026-01-18 01:58:48 +0530

    Add files via upload

---
+++ Lab-Work-6-pattern-printing_nested-loops/question5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    //n must be an odd number for the program to give desired result
+    int n;
+    char c=97;
+    int x;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    printf("Enter a number to show in the pattern");
+    scanf("%d",&x);
+    for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= n; j++){
+
+        if(j <= i)            
+            printf("%c", c);
+        else{
+            printf("%d", x);
+            x--;
+        }
+    }
+    x=4-i;
+    printf("\n");
+    c++;
+}
+
+}
